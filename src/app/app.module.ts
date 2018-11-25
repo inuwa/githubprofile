d9518767675961d9344b0aa42aa5790aa964f2d3
Shehu Inuwa <inuwa1@MacBook-Pro-2.local>
2018-11-25 21:08:36 +0000

    Initial deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import { NavigationComponent } from './shared/navigation/navigation.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { ProfileComponent } from './profile/profile.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavigationComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
